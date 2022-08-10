0ae335384d1b8837ad101b877c7f0a9e1bcf9490
Vladislav1300 <askerko1997@gmail.com>
2022-08-10 16:02:53 +0300

    init: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        arr.push(expr.slice(i, i + 10));
+    }
+    
+    let morse_arr = [];
+
+    for (i in arr) {
+
+        let morse_letter = '';
+
+        for (let j = 0; j < arr[i].length; j+=2) {
+            let n = arr[i].slice(j, j + 2);
+            if(n === '10') morse_letter += '.';
+            else if(n === '11') morse_letter += ('-');
+        }   
+        morse_arr.push(morse_letter);
+    }
+
+    let result = '';
+
+    for (letter in morse_arr){
+        if(MORSE_TABLE[morse_arr[letter]]) result += MORSE_TABLE[morse_arr[letter]];
+        else result += ' ';
+    }
+    
+    return result;
 }
 
 module.exports = {
